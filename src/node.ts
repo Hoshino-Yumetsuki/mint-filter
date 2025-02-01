b8295e7a6d509d950303758b7cdf00c0874229da
Hoshino-Yumetsuki <hoshino-yumetsuki@outlook.com>
2025-02-01 23:47:23 +0800

    .

--- src/node.ts
+++ src/node.ts
@@ -8,23 +8,23 @@
  */
 
 export default class Node {
-  // 深度
-  public depth = 0;
-  // 节点值
-  public key: string;
-  // 是否为单词最后节点
-  public word = false;
-  // 父节点的引用
-  // public parent?: Node;
-  // 子节点的引用（goto表）
-  public children: Record<string, Node> = {};
-  // failure表，用于匹配失败后的跳转
-  public fail?: Node;
-  // 引用计数
-  public count = 0;
+    // 深度
+    public depth = 0
+    // 节点值
+    public key: string
+    // 是否为单词最后节点
+    public word = false
+    // 父节点的引用
+    // public parent?: Node;
+    // 子节点的引用（goto表）
+    public children: Record<string, Node> = {}
+    // failure表，用于匹配失败后的跳转
+    public fail?: Node
+    // 引用计数
+    public count = 0
 
-  constructor(key: string, depth = 0) {
-    this.key = key;
-    this.depth = depth;
-  }
+    constructor(key: string, depth = 0) {
+        this.key = key
+        this.depth = depth
+    }
 }
